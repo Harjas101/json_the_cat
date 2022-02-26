1e1fe736ceb9bac576e4a076783828240b4be4d0
Harjas101 <anand.harjas@gmail.com>
2022-02-25 21:26:55 -0500

    first commit

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const fs = require("fs");
+const searchTerm = process.argv[2];
+request(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, (error, response, body) => {
+    if (error) {
+      console.log("this isnt working");
+      return;
+    }
+    const data = JSON.parse(body);
+    let breed = data[0];
+    if (breed) {
+      console.log(breed.description);
+    } else {
+      console.log("we got it for you");
+    }
+  }
+);
